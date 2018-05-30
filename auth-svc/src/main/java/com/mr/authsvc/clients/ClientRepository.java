efe0aa41405101f90aba4420759444249fd4f725
Matias Ramos <matiasr.mdq@gmail.com>
2018-05-30 01:35:05 -0300

    Adds authentication service and client

--- auth-svc/src/main/java/com/mr/authsvc/clients/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mr.authsvc.clients;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-  Optional<Client> findByClientId(String clientId);
-}
--- reservation-client/src/main/java/com/mr/reservationclient/ReservationClientApplication.java
+++ reservation-client/src/main/java/com/mr/reservationclient/ReservationClientApplication.java
@@ -4,16 +4,25 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Collection;
 import java.util.stream.Collectors;
@@ -24,6 +33,31 @@ import java.util.stream.Stream;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ReservationClientApplication {
+
+  @Bean
+    RestTemplate restTemplate(OAuth2AuthorizedClientService clientService) {
+    return new RestTemplateBuilder()
+        .interceptors(
+            (ClientHttpRequestInterceptor)
+                (httpRequest, bytes, execution) -> {
+                  OAuth2AuthenticationToken token =
+                      OAuth2AuthenticationToken.class.cast(
+                          SecurityContextHolder.getContext().getAuthentication());
+
+                  OAuth2AuthorizedClient client =
+                      clientService.loadAuthorizedClient(
+                          token.getAuthorizedClientRegistrationId(), token.getName());
+
+                  httpRequest
+                      .getHeaders()
+                      .add(
+                          HttpHeaders.AUTHORIZATION,
+                          "Bearer " + client.getAccessToken().getTokenValue());
+
+                  return execution.execute(httpRequest, bytes);
+                })
+        .build();
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(ReservationClientApplication.class, args);
@@ -43,8 +77,7 @@ class ReservationApiAdapterRestController {
   }
 
   public Collection<String> fallback() {
-    return Stream.of("This", "is", "data", "from", "fallback")
-            .collect(Collectors.toList());
+    return Stream.of("This", "is", "data", "from", "fallback").collect(Collectors.toList());
   }
 
   @HystrixCommand(fallbackMethod = "fallback")
